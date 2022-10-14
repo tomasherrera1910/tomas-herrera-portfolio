78262e581eaa4ca522d3d862649b5adc3af28f22
tomasherrera1910 <tomymza10@gmail.com>
2022-10-13 21:23:48 -0300

    actualizando CV y sección de proyectos

--- index.js
+++ index.js
@@ -122,25 +122,25 @@ addEventListener("scroll", () => {
 })
 const swiper = new Swiper(".swiper", {
   // Default parameters
-  // slidesPerView: 1,
-  // spaceBetween: 10,
-  // breakpoints: {
-  //   // when window width is >= 320px
-  //   320: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 20,
-  //   },
-  //   // when window width is >= 480px
-  //   480: {
-  //     slidesPerView: 2,
-  //     spaceBetween: 30,
-  //   },
-  //   // when window width is >= 640px
-  //   640: {
-  //     slidesPerView: 3,
-  //     spaceBetween: 40,
-  //   },
-  // },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
   // Optional parameters
   direction: "horizontal",
   loop: true,
